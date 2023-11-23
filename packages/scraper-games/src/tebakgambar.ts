afa4a66762348bd82de1e8dbfd1f0f7ef5f0b92f
Riissss <121730144+Riissss@users.noreply.github.com>
2023-11-23 13:52:28 +0700

    Update tebakgambar.ts

--- packages/scraper-games/src/tebakgambar.ts
+++ packages/scraper-games/src/tebakgambar.ts
@@ -5,7 +5,7 @@ export let tebakgambarjson: TebakGambar[]
 export default async function tebakgambar (): Promise<TebakGambar> {
   if (!tebakgambarjson) {
     tebakgambarjson = await got(
-      'https://raw.githubusercontent.com/BochilTeam/database/master/games/tebakgambar.json'
+      'https://raw.githubusercontent.com/Riissss/db/master/games/tebakgambar.json'
     ).json()
   }
   return TebakGambarSchema.parse(
